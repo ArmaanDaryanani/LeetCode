926cfae312f57c409c16617e369abf9b0760bf4a
armaandaryanani <37964722+ArmaanDaryanani@users.noreply.github.com>
2023-08-30 16:40:33 -0500

    Time: 14 ms (21.73%), Space: 44 MB (17.88%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,39 +1,12 @@
 class Solution {
-    public String intToRoman(int num) 
-    {
-        Map<Integer, String> map = new HashMap<>();
-        map.put(1, "I");
-        map.put(4, "IV");
-        map.put(5, "V");
-        map.put(9, "IX");
-        map.put(10, "X");
-        map.put(40, "XL");
-        map.put(50, "L");
-        map.put(90, "XC");
-        map.put(100, "C");
-        map.put(400, "CD");
-        map.put(500, "D");
-        map.put(900, "CM");
-        map.put(1000, "M");
-        int numMut =  num;
-        int total = 0;
-        String romanStr = "";
-        while (total != num) {
-            int maxKey = 0;
-            for (Integer key : map.keySet()) {
-                if (key <= numMut && key > maxKey) {
-                    maxKey = key;
-                }
-            }
+public String intToRoman(int num) 
+{
+        String[] ones = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String[] tens = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String[] hrns = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String[] ths ={"","M","MM","MMM"};
+        
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
 
-            if (maxKey > 0) {
-                romanStr += map.get(maxKey);
-                total += maxKey;
-                numMut -= maxKey;
-                continue;
-            }
-            break;
-        }
-        return romanStr;
-    }
+} 
 }
