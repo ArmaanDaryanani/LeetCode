126a83fbc8fc8e4c89a7228ed1aac0e5b17ec0fd
armaandaryanani <37964722+ArmaanDaryanani@users.noreply.github.com>
2023-09-08 16:52:12 -0500

    Time: 0 ms (100%), Space: 41.5 MB (17.69%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,6 +1,9 @@
 class Solution 
 {
+    //Output list
     ArrayList<String> output = new ArrayList<>();
+
+    //Defined static Mapping for memory and runtime efficiency
     static final Map<Character, String> map = new HashMap<>();
     static 
     {
@@ -13,32 +16,50 @@ class Solution
         map.put('8', "tuv");
         map.put('9', "wxyz");
     }
-
+    
     public List<String> letterCombinations(String digits) 
     {
+        //If list is empty
         if(digits.isEmpty())
         {
             return List.of();
         }
         
+        //Recursive method call
         combos(new StringBuilder(), digits, 0);
+
+        //Return list
         return output;
     }
+
+    //StringBuilder modifies existing object on recursion rather
+    //than string concatenation which creates new string objects
+    //each time. Passing index(rather than substring modification)
+    //elimates extra object creation(for substring case)
     public void combos(StringBuilder combo, String digits, int index)
     {
+        //if end of combination is reached, add to output list
         if (index == digits.length())
         {
             output.add(combo.toString());
             return;
         }
+        //String object for current mapping
         String str = map.get(digits.charAt(index));
+        //char iteration
         for (char ch : str.toCharArray())
         {
+            //append to running combination
             combo.append(ch);  
+            //increment index and run method again
             combos(combo, digits, index + 1);
+            //for backtracking process
             combo.deleteCharAt(combo.length() - 1);  
         }
     }
+    //The following recursive algorithm has been optimizied
+    //for low memory and runtime-intensive applications.
+    
 
 
 
